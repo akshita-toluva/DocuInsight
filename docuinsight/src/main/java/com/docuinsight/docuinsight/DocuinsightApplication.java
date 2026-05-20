0f625ee2af4be01863e265401a3436435f8efbb2
akshitatoluva <akshita.toluva@zemosolabs.com>
2026-05-20 23:24:24 +0530

    "Initial commit with Spring Boot auth setup"

---
+++ docuinsight/src/main/java/com/docuinsight/docuinsight/DocuinsightApplication.java
@@ -0,0 +1,14 @@
+package com.docuinsight.docuinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuinsightApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
